bb14f4388b493a584d4bb478b7254a9da11547a8
Nitin <52646268+Nitin1818@users.noreply.github.com>
2020-10-03 10:32:49 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -114,12 +114,10 @@ WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
 
 # Load at end to ensure all prev variables have been set
-from haruka.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, GbanLockHandler
+from haruka.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
 
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
 
 if ALLOW_EXCL:
     tg.CommandHandler = CustomCommandHandler
-
-tg.CommandHandler = GbanLockHandler
